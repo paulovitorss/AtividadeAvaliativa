a05a09f80205384c0e0fdd87ae4e73f8cabbb830
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-06-23 19:18:16 -0400

    fix: update Retrofit base URL

--- app/src/main/java/br/com/unemat/paulo/atividadeavaliativa/data/remote/RetrofitClient.java
+++ app/src/main/java/br/com/unemat/paulo/atividadeavaliativa/data/remote/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.70.200.122:8080/";
+    private static final String BASE_URL = "http://192.168.18.76:8080/";
     private static volatile Retrofit retrofit = null;
 
     public static Retrofit getClient(Context context) {
